972bcdb006be913b0796e641eb3eb905c519a43c
Jad Saad <jadsaad06@gmail.com>
2024-09-14 21:27:25 -0700

    Accomadated for years that are divisible by 100 and 4 in leapYears function

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,7 +1,9 @@
 const leapYears = function(year) {
     let isLeapYear = false;
 
-    if(year % 400 == 0 || year % 4 == 0){
+    if(year % 400 == 0){
+        isLeapYear = true;
+    }else if(year % 4 == 0 && year % 100 != 0){
         isLeapYear = true;
     }
 
