109540473d3f23147c07aab70312800372884468
Jad Saad <jadsaad06@gmail.com>
2024-09-14 20:59:34 -0700

    Fixed the removeFromArray function. Encorporated "..." before var to allow for multiple inputs

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...valuesToRemove) {
+    for(let i = 0; i < arr.length; i++){
+        if(valuesToRemove.includes(arr[i])){
+            arr.splice(i,1);
+            i--;
+        }
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
