2e66483ce5c1548d8fd87f59db012cb6275511d3
Jad Saad <jadsaad06@gmail.com>
2024-09-14 21:41:37 -0700

    fixed the rounding to output an integer rather than a string

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,15 +1,15 @@
 const convertToCelsius = function(fTemp) {
   let celciusConv = (fTemp - 32) * (5/9);
 
-  if(!Number.isInteger(celciusConv)) celciusConv = celciusConv.toFixed(1);
-  
+  if(!Number.isInteger(celciusConv)) celciusConv = Math.round(celciusConv * 10)/10;
+
   return celciusConv;
 };
 
 const convertToFahrenheit = function(cTemp) {
   let fahrenheitConv = (cTemp/(5/9)) + 32;
 
-  if(!Number.isInteger(fahrenheitConv)) fahrenheitConv = fahrenheitConv.toFixed(1);
+  if(!Number.isInteger(fahrenheitConv)) fahrenheitConv = Math.round(fahrenheitConv * 10) / 10;
 
   return fahrenheitConv;
 };
