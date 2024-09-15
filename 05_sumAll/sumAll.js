956bca2250c33a00cf860f36348cc4a56cb88ab6
Jad Saad <jadsaad06@gmail.com>
2024-09-14 21:21:07 -0700

    Made a change to sumAll.js to allow for the large number to go first

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,7 +1,10 @@
 const sumAll = function(num1, num2) {
     if(Number.isInteger(num1) && Number.isInteger(num2) && num1 >= 0 && num2 >= 0){
         let ans = 0;
-        for(let i = num1; i <= num2; i++){
+        let finish = Math.max(num1, num2);
+        let start = Math.min(num1, num2);
+
+        for(let i = start; i <= finish; i++){
             ans += i;
         }
         return ans;
