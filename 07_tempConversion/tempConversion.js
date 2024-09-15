4521d83de98b733e58905ed349973d148589eedb
Jad Saad <jadsaad06@gmail.com>
2024-09-14 21:37:51 -0700

    Added celcius and fahrenheit conversions as well as rounding if the conversion is not whole

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  let celciusConv = (fTemp - 32) * (5/9);
+
+  if(!Number.isInteger(celciusConv)) celciusConv = celciusConv.toFixed(1);
+  
+  return celciusConv;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  let fahrenheitConv = (cTemp/(5/9)) + 32;
+
+  if(!Number.isInteger(fahrenheitConv)) fahrenheitConv = fahrenheitConv.toFixed(1);
+
+  return fahrenheitConv;
 };
 
 // Do not edit below this line
