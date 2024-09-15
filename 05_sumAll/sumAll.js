4b9162b1c150815593aa515a1e8efd0acef64625
Jad Saad <jadsaad06@gmail.com>
2024-09-14 21:10:02 -0700

    Finished sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if(Number.isInteger(num1) && Number.isInteger(num2) && num1 >= 0 && num2 >= 0){
+        let ans = 0;
+        for(let i = num1; i <= num2; i++){
+            ans += i;
+        }
+        return ans;
+    }else{
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
